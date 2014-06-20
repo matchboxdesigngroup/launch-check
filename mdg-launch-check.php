984a9a0cdeed01931baffe305ec36d0eaa9ebad5
Dan Holloran <dholloran@matchboxdesigngroup.com>
2014-06-20 17:09:24 -0500

    Cleaned up functions and added wp_kses escaping for all content

--- mdg-launch-check.php
+++ mdg-launch-check.php
@@ -24,59 +24,93 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-	
+
 /**
  * Display an error message when the blog is set to private.
+ *
+ * @return void
  */
 function mdg_is_blog_public() {
-	$blog_public 	= get_option( 'blog_public' );
-		
+	$blog_public = get_option( 'blog_public' );
 
-	if ( $blog_public == 1 ){
-		return;	
+	if ( $blog_public == 1 ) {
+		return;
 	}
 
-	echo "<div id='message' class='error'>";
-	echo "<p><strong>" . __( "Huge SEO Issue: You're blocking access to robots.", 'mdg-launch-check' ) . "</strong> " . sprintf( __( "You must %sgo to your Reading Settings%s and uncheck the box for Search Engine Visibility.", 'mdg-launch-check' ), "<a href='" . admin_url( 'options-reading.php' ) . "'>", "</a>" ) . "</p></div>";
+	$alert  = '';
+	$alert .= '<div id="message" class="error">';
+	$alert .= '<p>';
+	$alert .= '<strong>' . __( "Huge SEO Issue: You're blocking access to robots.", 'mdg-launch-check' ) . '</strong> ';
+	$alert .= sprintf( __( 'You must %sgo to your Reading Settings%s and uncheck the box for Search Engine Visibility.', 'mdg-launch-check' ), '<a href="' . admin_url( 'options-reading.php' ) . '">', '</a>' );
+	$alert .= '</p></div>';
+
+	echo wp_kses( $alert, 'post' );
 }
+
+
 
 /**
  * Display an error message when using the default WordPress tagline.
+ *
+ * @return void
  */
 function mdg_check_blog_description() {
-	$default_desc	= get_option( 'blogdescription');
-	
-		if ( $default_desc != 'Just another WordPress site' ){
-			return;	
-		}
+	$default_desc = get_option( 'blogdescription' );
 
-		echo "<div id='message' class='error'>";
-		echo "<p><strong>" . __( "Whoa there partner: You're Still using the default tagline.", 'mdg-launch-check' ) . "</strong> " . sprintf( __( "You must %sgo to your General Settings%s and change the Tagline to a description for this site.", 'mdg-launch-check' ), "<a href='" . admin_url( 'options-general.php' ) . "'>", "</a>" ) . "</p></div>";
+	if ( $default_desc != 'Just another WordPress site' ) {
+		return;
+	}
+
+	$alert  = '';
+	$alert .= '<div id="message" class="error">';
+	$alert .= '<p><strong>';
+	$alert .= __( "Whoa there partner: You're Still using the default tagline.", 'mdg-launch-check' );
+	$alert .= '</strong> ';
+	$alert .= sprintf( __( 'You must %sgo to your General Settings%s and change the Tagline to a description for this site.', 'mdg-launch-check' ), '<a href="' . admin_url( 'options-general.php' ) . '">', '</a>' );
+	$alert .= '</p></div>';
+
+	echo wp_kses( $alert, 'post' );
 }
+
+
 
 /**
  * Check to see if the Google Analytics for WordPress plugin is installed.
+ *
+ * @return void
  */
 function mdg_check_analytics_plugin() {
-	$default_desc	= get_option( 'blogdescription');
-		if ( is_plugin_active( 'universal-analytics/universalanalytics.php' ) ) {
-			return;	
-		}
+	$default_desc = get_option( 'blogdescription' );
+	if ( is_plugin_active( 'universal-analytics/universalanalytics.php' ) ) {
+		return;
+	}
 
-		echo "<div id='message' class='error'>";
-		echo "<p><strong>" . __( "Easy killer: You're not tracking your site yet.", 'mdg-launch-check' ) . "</strong> " . sprintf( __( "How are you going to track those awesome visitors? Go %sinstall the Universal Analytics plugin%s now.", 'mdg-launch-check' ), "<a href='" . admin_url( 'plugin-install.php?tab=plugin-information&plugin=universal-analytics&TB_iframe=true' ) . "'class='thickbox'>", "</a>" ) . "</p></div>";
+	$alert  = '';
+	$alert .= '<div id="message" class="error">';
+	$alert .= '<p><strong>';
+	$alert .= __( "Easy killer: You're not tracking your site yet.", 'mdg-launch-check' );
+	$alert .= '</strong> ';
+	$alert .= sprintf( __( 'How are you going to track those awesome visitors? Go %sinstall the Universal Analytics plugin%s now.', 'mdg-launch-check' ), '<a href="' . admin_url( 'plugin-install.php?tab=plugin-information&plugin=universal-analytics&TB_iframe=true' ) . '" class="thickbox">', '</a>' );
+	$alert .= '</p></div>';
+
+	echo wp_kses( $alert, 'post' );
 }
 
 
-  //plugin is activated
 
+/**
+ * Plugin setup
+ *
+ * @return  void
+ */
 function mdg_admin_check() {
 	// To disable screens from displaying the alerts add the current screens base to the disabled screens
 	$current_screen   = get_current_screen();
 	$disabled_screens = array(
-		'plugin-install',  	// Hide the message on the Universal Analytics popup
-		'update',						// Hide the message when updating plugins
+		'plugin-install',
+		'update',
 	);
+
 	if ( in_array( $current_screen->base, $disabled_screens ) ) {
 		return;
 	} // if()
@@ -84,6 +118,5 @@ function mdg_admin_check() {
 	mdg_check_blog_description();
 	mdg_is_blog_public();
 	mdg_check_analytics_plugin();
-	
 }
 add_action( 'admin_head', 'mdg_admin_check' );
